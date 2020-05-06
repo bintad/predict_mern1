512c4bd05b9a08517add4a94450a36b4646db5dd
binta diallo <bintadalen2009@gmail.com>
2020-05-06 15:36:54 -0700

    predict1

---
+++ hoist.js
@@ -0,0 +1,55 @@
+console.log(hello);                                   
+var hello = 'world';                                 
+var needle = 'haystack';
+test();
+function test(){
+    var needle = 'magnet';
+    console.log(needle);
+}
+var binta = 'so tired';
+function print(){
+    binta= 'only okay';
+    console.log(binta);
+}
+console.log(binta);
+
+var food = 'fish';
+console.log(food);
+eat();
+function eat(){
+    food = 'saumon';
+    console.log(food);
+    var food = 'gone';
+}
+
+mean();
+console.log(food);
+var mean = function() {
+    food = "fish";
+    console.log(food);
+    var food = "fish";
+    console.log(food);
+}
+console.log(food);
+
+console.log(genre);
+var genre = "danse";
+rewind();
+function rewind() {
+    genre = "salsa";
+    console.log(genre);
+    var genre = "r&b";
+    console.log(genre);
+}
+console.log(genre);
+
+dojo = "bellevue";
+console.log(dojo);
+learn();
+function learn() {
+    dojo = "seattle";
+    console.log(dojo);
+    var dojo = "kent";
+    console.log(dojo);
+}
+console.log(dojo);
